f24fd196954f625d0e70740cf13e20ead9a3962f
namraaslam <namra.aslam@ucalgary.ca>
2023-02-15 21:31:16 -0700

    updated and refactored middle.js and middleTest.js

--- middle.js
+++ middle.js
@@ -1,44 +1,14 @@
 const assertArraysEqual = require('./assertArraysEqual');
 
-const middle = function (arr1) {
-
-  let midArray = [];
-  let arrLength = arr1.length;
-  let mid = Math.floor(arrLength / 2);
-  if (arrLength <= 2) {
-      return midArray;
-
+const middle = function(array){
+    if (array.length <= 2) 
+      return [];
+      let i = Math.floor(array.length /2);
+      if (array.length % 2 == 0) {
+        return [array[i - 1], array[i]];
+      } else {
+        return [array[i]]
+      }   
   }
-
-  else {
-      if (arrLength % 2 === 0) {
-          midArray.push(arr1[mid] - 1);
-          midArray.push(arr1[mid]);
-      }
-      else {
-          midArray.push(arr1[mid]);
-
-      }
-
-  }
-
-  return midArray;
-
-}
-
-
-
-//Test Cases
-//empty
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-//odd
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-//even
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([3, 1, 1, 2]), [1, 1]);
-
 
 module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -2,15 +2,16 @@ const assertArraysEqual = require("../assertArraysEqual")
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 
-
-//Test Cases
+//Test Case
 //empty
+console.log('Empty');
 assertArraysEqual(middle([1]), []); // => []
 assertArraysEqual(middle([1, 2]), []); // => []
 //odd
+console.log('Odd');
 assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
 //even
+console.log('Even');
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([3, 1, 1, 2]), [1, 1]);
